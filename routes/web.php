2b07b26d123b0bab38af1ff387ba207994f7ce6b
MR <masudranamd112@gmail.com>
2026-03-12 02:44:53 +0600

    ok

--- routes/web.php
+++ routes/web.php
@@ -137,7 +137,7 @@ Route::prefix('admin')->name('admin.')->middleware('admin.auth')->group(function
     Route::get('/categories', [AdminProductsController::class, 'categories'])->name('categories');
     Route::get('/order-call', [OrderCallController::class, 'index'])->name('order-call');
 
-    // orders 
+    // orders
     Route::get('/orders', [AdminOrderController::class, 'orders'])->name('orders');
     Route::get('/orders/{orderId}', [AdminOrderController::class, 'view'])->name('orders.view');
     Route::post('/orders/{orderId}/discount', [AdminOrderController::class, 'applyDiscount'])->name('orders.discount.apply');
@@ -167,9 +167,11 @@ Route::prefix('admin')->name('admin.')->middleware('admin.auth')->group(function
     Route::get('/competition/{competitor}/view', [CompetitionController::class, 'view'])->name('competition.view');
     Route::get('/coach', [CoachController::class, 'index'])->name('coach');
     Route::get('/courier', [CourierController::class, 'index'])->name('courier');
-    Route::get('/billing', [DashboardController::class, 'billing'])->name('billing');
 
-    // Shop Settings 
+    // package
+    Route::get('/packages', [AdminPackageController::class, 'packages'])->name('packages');
+
+    // Shop Settings
     Route::get('/shop-settings', [ShopSettingsController::class, 'index'])->name('shop-settings');
     Route::get('/shop-settings/domain', [ShopSettingsController::class, 'domain'])->name('shop-settings.domain');
     Route::get('/shop-settings/theme', [ShopSettingsController::class, 'theme'])->name('shop-settings.theme');
