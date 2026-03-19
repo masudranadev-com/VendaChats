a56a8a430ec7945c0d430c6faf6e2b24b3abb206
MR PC <avengers11official@gmail.com>
2026-03-19 09:12:16 +0600

    ok

--- resources/views/ecommerce/theme1/contact.blade.php
+++ resources/views/ecommerce/theme1/contact.blade.php
@@ -80,9 +80,9 @@
                         <div class="row g-4 align-items-center justify-content-center">
                             <div class="col-md-6 col-lg-6 col-xl-3 wow fadeInUp" data-wow-delay="0.1s">
                                 <div class="rounded p-4">
-                                    <div class="rounded-circle bg-secondary d-flex align-items-center justify-content-center mb-4"
+                                    <div class="rounded-circle bg-primary d-flex align-items-center justify-content-center mb-4"
                                         style="width: 70px; height: 70px;">
-                                        <i class="fas fa-map-marker-alt fa-2x text-primary"></i>
+                                        <i class="fas fa-map-marker-alt fa-2x theme-on-primary"></i>
                                     </div>
                                     <div>
                                         <h4>Address</h4>
@@ -92,9 +92,9 @@
                             </div>
                             <div class="col-md-6 col-lg-6 col-xl-3 wow fadeInUp" data-wow-delay="0.3s">
                                 <div class="rounded p-4">
-                                    <div class="rounded-circle bg-secondary d-flex align-items-center justify-content-center mb-4"
+                                    <div class="rounded-circle bg-primary d-flex align-items-center justify-content-center mb-4"
                                         style="width: 70px; height: 70px;">
-                                        <i class="fas fa-envelope fa-2x text-primary"></i>
+                                        <i class="fas fa-envelope fa-2x theme-on-primary"></i>
                                     </div>
                                     <div>
                                         <h4>Mail Us</h4>
@@ -104,9 +104,9 @@
                             </div>
                             <div class="col-md-6 col-lg-6 col-xl-3 wow fadeInUp" data-wow-delay="0.5s">
                                 <div class="rounded p-4">
-                                    <div class="rounded-circle bg-secondary d-flex align-items-center justify-content-center mb-4"
+                                    <div class="rounded-circle bg-primary d-flex align-items-center justify-content-center mb-4"
                                         style="width: 70px; height: 70px;">
-                                        <i class="fa fa-phone-alt fa-2x text-primary"></i>
+                                        <i class="fa fa-phone-alt fa-2x theme-on-primary"></i>
                                     </div>
                                     <div>
                                         <h4>Telephone</h4>
@@ -116,9 +116,9 @@
                             </div>
                             <div class="col-md-6 col-lg-6 col-xl-3 wow fadeInUp" data-wow-delay="0.7s">
                                 <div class="rounded p-4">
-                                    <div class="rounded-circle bg-secondary d-flex align-items-center justify-content-center mb-4"
+                                    <div class="rounded-circle bg-primary d-flex align-items-center justify-content-center mb-4"
                                         style="width: 70px; height: 70px;">
-                                        <i class="fab fa-firefox-browser fa-2x text-primary"></i>
+                                        <i class="fab fa-firefox-browser fa-2x theme-on-primary"></i>
                                     </div>
                                     <div>
                                         <h4>Yoursite@ex.com</h4>
--- resources/views/ecommerce/theme1/master.blade.php
+++ resources/views/ecommerce/theme1/master.blade.php
@@ -281,56 +281,6 @@
     <!-- Footer Start -->
     <div class="container-fluid footer py-5 wow fadeIn" data-wow-delay="0.2s">
         <div class="container py-5">
-            <div class="row g-4 rounded mb-5" style="background: rgba(255, 255, 255, .03);">
-                <div class="col-md-6 col-lg-6 col-xl-3">
-                    <div class="rounded p-4">
-                        <div class="rounded-circle bg-secondary d-flex align-items-center justify-content-center mb-4"
-                            style="width: 70px; height: 70px;">
-                            <i class="fas fa-map-marker-alt fa-2x text-primary"></i>
-                        </div>
-                        <div>
-                            <h4 class="text-white">Address</h4>
-                            <p class="mb-2">123 Street New York.USA</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6 col-lg-6 col-xl-3">
-                    <div class="rounded p-4">
-                        <div class="rounded-circle bg-secondary d-flex align-items-center justify-content-center mb-4"
-                            style="width: 70px; height: 70px;">
-                            <i class="fas fa-envelope fa-2x text-primary"></i>
-                        </div>
-                        <div>
-                            <h4 class="text-white">Mail Us</h4>
-                            <p class="mb-2">info@example.com</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6 col-lg-6 col-xl-3">
-                    <div class="rounded p-4">
-                        <div class="rounded-circle bg-secondary d-flex align-items-center justify-content-center mb-4"
-                            style="width: 70px; height: 70px;">
-                            <i class="fa fa-phone-alt fa-2x text-primary"></i>
-                        </div>
-                        <div>
-                            <h4 class="text-white">Telephone</h4>
-                            <p class="mb-2">(+012) 3456 7890</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6 col-lg-6 col-xl-3">
-                    <div class="rounded p-4">
-                        <div class="rounded-circle bg-secondary d-flex align-items-center justify-content-center mb-4"
-                            style="width: 70px; height: 70px;">
-                            <i class="fab fa-firefox-browser fa-2x text-primary"></i>
-                        </div>
-                        <div>
-                            <h4 class="text-white">Yoursite@ex.com</h4>
-                            <p class="mb-2">(+012) 3456 7890</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
             <div class="row g-5">
                 <div class="col-md-6 col-lg-6 col-xl-3">
                     <div class="footer-item d-flex flex-column">
